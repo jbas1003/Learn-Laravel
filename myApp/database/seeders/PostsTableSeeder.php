52dd03c2adb2ea5b26287f95dc4875f277d32e63
Jon <joncaesar@gmail.com>
2023-07-11 15:07:15 +0800

    Seeder
    Defining our Seeder
    Defining our Post seeder in the DatabaseSeeder

--- myApp/database/seeders/PostsTableSeeder.php
+++ myApp/database/seeders/PostsTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PostsTableSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $posts = [
+            [
+                'title' => 'Post One',
+                'excerpt' => 'Summary of Post One',
+                'body' => 'Body of Post One',
+                'image_path' => 'Empty',
+                'is_published' => false,
+                'min_to_read' => 2,
+            ],
+            [
+                'title' => 'Post Two',
+                'excerpt' => 'Summary of Post Two',
+                'body' => 'Body of Post Two',
+                'image_path' => 'Empty',
+                'is_published' => false,
+                'min_to_read' => 2,
+            ]
+        ];
+
+        foreach($posts as $key => $value) {
+            Post::create($value);
+        }
     }
 }
