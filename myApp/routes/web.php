5d119c022577e2d5b54396b279961fbffa1238c5
Jon <joncaesar@gmail.com>
2023-07-11 08:26:30 +0800

    Update web.php

--- myApp/routes/web.php
+++ myApp/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
