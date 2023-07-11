88e9cbd2338ad342aa8195857e742e0c82be808b
Jon <joncaesar@gmail.com>
2023-07-11 08:22:19 +0800

    Revert "Defining first Route"
    This reverts commit e618342cc9da2f41b04050e5172a5ffb35262285.

--- myApp/routes/web.php
+++ myApp/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/blog', [PostsController::class, 'index']);
 
 require __DIR__.'/auth.php';
