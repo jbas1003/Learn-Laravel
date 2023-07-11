4a39859558a5c3fa21229b0f2e1fba4ab82c592d
Jon <joncaesar@gmail.com>
2023-07-11 08:54:35 +0800

    Making Route Parameter
    Making Route Parameter
    Making Route Parameter Optional

--- myApp/app/Http/Controllers/PostsController.php
+++ myApp/app/Http/Controllers/PostsController.php
@@ -33,9 +33,9 @@ class PostsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        return $id;
     }
 
     /**
